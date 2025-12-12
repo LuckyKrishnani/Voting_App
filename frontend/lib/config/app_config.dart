606b70191da921c6a811971dba1221e6dd8af230
LuckyKrishnani <luckyaneelsoni7878@gmail.com>
2025-12-12 22:05:28 +0500

    Delete frontend directory

--- frontend/lib/config/app_config.dart
+++
@@ -1,8 +0,0 @@
-class AppConfig {
-  static const String appName = 'Voting Bank';
-  static const String apiBaseUrl = 'http://localhost:8080/api/v1';
-  static const int apiTimeout = 30000; // 30 seconds
-  static const String jwtTokenKey = 'jwt_token';
-  static const String refreshTokenKey = 'refresh_token';
-  static const String userKey = 'user_data';
-}
